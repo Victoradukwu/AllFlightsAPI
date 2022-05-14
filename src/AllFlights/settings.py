def2882e30322fd4b25145e2e039313cf774168e
Victor <vicads01@gmail.com>
2022-05-14 18:50:37 +0100

    add custom default exception handler

--- src/AllFlights/settings.py
+++ src/AllFlights/settings.py
@@ -61,6 +61,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny"
     ],
+    'EXCEPTION_HANDLER': 'app.utils.custom_exception_handler',
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
