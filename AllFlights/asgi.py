7193c38655622373bf5ba86c81db81d1c9d08d5a
Victor <vicads01@gmail.com>
2022-11-14 05:55:48 +0100

    fix DJANGO_SETTINGS_MODULE on Heroku 3

--- AllFlights/asgi.py
+++ AllFlights/asgi.py
@@ -6,11 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+import os
 
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf import settings
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AllFlights.settings')
+django.setup()
 
 from app import routing
 
